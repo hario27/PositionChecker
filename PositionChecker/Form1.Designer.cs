15b5203dd74a62a65a8293da764f81dee04c5ddf
harima yoshiyuki <yharima@4cast.co.jp>
2019-11-26 19:00:43 +0900

    全部コミット

---
+++ PositionChecker/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace PositionChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.設定EToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cSSフォーマットAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 25);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(5, 573);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(854, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(1006, 34);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 59);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "切り出し";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(863, 572);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 30);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "コピー";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(19, 80);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 38);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "矩形＋イメージ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem,
+            this.設定EToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1147, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了XToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 終了XToolStripMenuItem
+            // 
+            this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+            this.終了XToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.終了XToolStripMenuItem.Text = "終了(&X)";
+            this.終了XToolStripMenuItem.Click += new System.EventHandler(this.終了XToolStripMenuItem_Click);
+            // 
+            // 設定EToolStripMenuItem
+            // 
+            this.設定EToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cSSフォーマットAToolStripMenuItem});
+            this.設定EToolStripMenuItem.Name = "設定EToolStripMenuItem";
+            this.設定EToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.設定EToolStripMenuItem.Text = "設定(&E)";
+            // 
+            // cSSフォーマットAToolStripMenuItem
+            // 
+            this.cSSフォーマットAToolStripMenuItem.Name = "cSSフォーマットAToolStripMenuItem";
+            this.cSSフォーマットAToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.cSSフォーマットAToolStripMenuItem.Text = "CSSフォーマット(&A)";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(19, 28);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 38);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "矩形";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(1006, 387);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 41);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "矩形を保存";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(986, 122);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(141, 137);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "削除";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 608);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "PositionCheker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 設定EToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cSSフォーマットAToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ PositionChecker/Form1.cs
@@ -0,0 +1,318 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PositionChecker
+{
+    public partial class Form1 : Form
+    {
+        // 矩形が入ったイメージを保存
+        public List<Mat> matList = new List<Mat>();
+        public string imageFolder = "";
+
+        // 矩形を保存
+        public List<Rect> rectList = new List<Rect>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            var filePath = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            var fileName = Path.GetFileName(filePath[0]);
+            var ext = Path.GetExtension(filePath[0]);
+
+            if(this.pictureBox1.Image == null)
+            {
+                if (ext == ".jpg")
+                {
+                    this.button1.Enabled = true;
+                    this.pictureBox1.Image = CreateImage(filePath[0]);
+                    Mat mat = BitmapConverter.ToMat(new Bitmap(this.pictureBox1.Image)).CvtColor(ColorConversionCodes.BGRA2BGR);
+                    matList = new List<Mat>() { mat };
+                    imageFolder = Path.GetDirectoryName(filePath[0]);
+                }
+            }
+            else
+            {
+                Mat mat = BitmapConverter.ToMat(new Bitmap(CreateImage(filePath[0]))).CvtColor(ColorConversionCodes.BGRA2BGR);
+                if(ext == ".jpg" && mat.Width == 960 && mat.Height == 540)
+                {
+                    Mat newMat = BitmapConverter.ToMat(new Bitmap(CreateImage(filePath[0]))).CvtColor(ColorConversionCodes.BGRA2BGR);
+                    matList = new List<Mat>() { newMat };
+                    Mat newMat1 = matList[0].Clone();
+                    rectList.ForEach(item =>
+                    {
+                        Cv2.Rectangle(newMat1, item, new Scalar(0, 255, 0), 2);
+                    });
+                    Image image = BitmapConverter.ToBitmap(newMat1);
+                    this.pictureBox1.Image = image;
+                    imageFolder = Path.GetDirectoryName(filePath[0]);
+                }
+                else
+                {
+                    this.button2.Enabled = true;
+                    this.button4.Enabled = true;
+                    createCv2Rectangle(filePath[0]);
+                }
+            }
+            
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(this.textBox1.Text != "")
+            {
+                Clipboard.SetText(this.textBox1.Text);
+            }
+        }
+
+        private void createCv2Rectangle(string filePath)
+        {
+            var fileName = Path.GetFileName(filePath);
+            var ext = Path.GetExtension(filePath);
+
+            if ((ext == ".png" || ext == ".jpg") && this.pictureBox1.Image != null)
+            {
+                Mat tempMat = matList[matList.Count - 1].Clone();
+
+                using (Mat mat = tempMat)
+                using (Mat temp = BitmapConverter.ToMat(new Bitmap(filePath)).CvtColor(ColorConversionCodes.BGRA2BGR))
+                using (Mat result = new Mat())
+                {
+                    if(temp.Width == 960 && temp.Height == 540)
+                    {
+                        return;
+                    }
+                    if(ext == ".jpg")
+                    {
+                        Cv2.Resize(temp, temp, new OpenCvSharp.Size(0.5 * temp.Width, 0.5 * temp.Height), 0, 0, InterpolationFlags.Lanczos4);
+                    }
+                    
+                    // テンプレートマッチ
+                    Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
+
+                    // 類似度が最大/最小となる画素の位置を調べる
+                    OpenCvSharp.Point minloc, maxloc;
+                    double minval, maxval;
+                    Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                    // しきい値で判断
+                    for (var i = 1.0; i > 0.3; i-=0.05)
+                    {
+                        if (maxval >= i)
+                        {
+
+                            // 最も見つかった場所に赤枠を表示
+                            Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
+                            rectList.Add(rect);
+                            rectList.ForEach(item =>
+                            {
+                                Cv2.Rectangle(mat, item, new Scalar(0, 255, 0), 2);
+                            });
+
+                            // ウィンドウに矩形を表示
+                            Image image = BitmapConverter.ToBitmap(mat);
+                            this.pictureBox1.Image = image;
+
+                            // CSSを表示
+                            int minX = 960;
+                            int maxX = 0;
+                            int minY = 540;
+                            int maxY = 0;
+
+                            rectList.ForEach(item =>
+                            {
+                                if (item.X < minX)
+                                {
+                                    minX = item.X;
+                                }
+                                if (item.X + item.Width > maxX)
+                                {
+                                    maxX = item.X + item.Width;
+                                }
+                                if (item.Y < minY)
+                                {
+                                    minY = item.Y;
+                                }
+                                if (item.Y + item.Height > maxY)
+                                {
+                                    maxY = item.Y + item.Height;
+                                }
+
+                            });
+
+                            this.textBox1.Text = string.Format(@"left:{0}px; top:{1}px; width:{2}px; height:{3}px;", minX, minY, maxX - minX, maxY - minY);
+
+                            return;
+                        }
+                    }
+                    // 見つからない
+                    MessageBox.Show("見つかりませんでした");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(rectList.Count == 0)
+            {
+                return;
+            }
+            int minX = 960;
+            int maxX = 0;
+            int minY = 540;
+            int maxY = 0;
+
+            rectList.ForEach(item =>
+            {
+                if(item.X < minX)
+                {
+                    minX = item.X;
+                }
+                if(item.X + item.Width > maxX)
+                {
+                    maxX = item.X + item.Width;
+                }
+                if(item.Y < minY)
+                {
+                    minY = item.Y;
+                }
+                if(item.Y + item.Height > maxY)
+                {
+                    maxY = item.Y + item.Height;
+                }
+                
+            });
+            /*
+            //SaveFileDialogクラスのインスタンスを作成
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            sfd.FileName = "bg.jpg";
+            //はじめに表示されるフォルダを指定する
+            sfd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            sfd.Filter = "JPGファイル(*.jpg;*.JPG)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            //2番目の「すべてのファイル」が選択されているようにする
+            sfd.FilterIndex = 2;
+            //タイトルを設定する
+            sfd.Title = "保存先のファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            sfd.RestoreDirectory = true;
+            //既に存在するファイル名を指定したとき警告する
+            //デフォルトでTrueなので指定する必要はない
+            sfd.OverwritePrompt = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            sfd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                Console.WriteLine(sfd.FileName);
+            }
+            */
+            System.IO.File.Delete(imageFolder + "\\temp.jpg");
+            var mat2 = matList[0].Clone(new Rect(minX, minY, maxX - minX, maxY - minY));
+            Cv2.ImWrite(imageFolder + "\\temp.jpg", mat2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button4.Enabled = false;
+            this.textBox1.Text = "";
+            this.pictureBox1.Image = null;
+            rectList = new List<Rect>();
+        }
+
+        public static System.Drawing.Image CreateImage(string filename)
+        {
+            System.IO.FileStream fs = new System.IO.FileStream(
+                filename,
+                System.IO.FileMode.Open,
+                System.IO.FileAccess.Read);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(fs);
+            fs.Close();
+            return img;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = Properties.Settings.Default.location;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.state = this.WindowState;
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                // ウインドウステートがNormalな場合には位置（location）とサイズ（size）を記憶する。
+                Properties.Settings.Default.location = this.Location;
+            }
+            else
+            {
+                // もし最小化（minimized）や最大化（maximized）の場合には、RestoreBoundsを記憶する。
+                Properties.Settings.Default.location = this.RestoreBounds.Location;
+            }
+
+            // ここで設定を保存する
+            Properties.Settings.Default.Save();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(this.pictureBox1.Image != null)
+            {
+                Mat mat = matList[0].Clone();
+                rectList.ForEach(item =>
+                {
+                    Cv2.Rectangle(mat, item, new Scalar(0, 255, 0), 2);
+                });
+                this.pictureBox1.Image = BitmapConverter.ToBitmap(mat);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.button2.Enabled = false;
+            this.button4.Enabled = false;
+            this.textBox1.Text = "";
+            rectList = new List<Rect>();
+            if(this.pictureBox1.Image != null)
+            {
+                Mat mat = matList[0].Clone();
+                this.pictureBox1.Image = BitmapConverter.ToBitmap(mat);
+            }
+        }
+
+        private void 終了XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
